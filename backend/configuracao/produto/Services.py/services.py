71483680dcde1a524564f3a6e329f76f753580e4
ellen samanta <ellen.samanta@outlook.com>
2024-12-07 12:27:41 -0300

    feat: [Services] Criando método para cadastrar um novo produto

--- backend/configuracao/produto/Services.py/services.py
+++ backend/configuracao/produto/Services.py/services.py
@@ -11,7 +11,17 @@ class ProductsService:
     def obterProduto(product_id):
         return get_object_or_404(Produto, id=product_id)
     
-
+    @staticmethod
+    def cadastrarProduto(request):
+        if request.method == 'POST':
+            nome = request.POST.get('nome')
+            preco = request.POST.get('preco')
+            quantidade = request.POST.get('quantidade')
+            categoria_id = request.POST.get('categoria')
+            estoque_minimo = request.POST.get('estoque_minimo')
+            estoque_maximo = request.POST.get('estoque_maximo')
+            Produto.objects.create(nome=nome, preco=preco, quantidade=quantidade, categoria_id=categoria_id, estoque_minimo=estoque_minimo, estoque_maximo=estoque_maximo)
+            return HttpResponseRedirect('/produto/')
         
 
 
